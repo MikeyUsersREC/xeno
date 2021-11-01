6f70ff5144a2a49c8f0756409fd873e8564da551
MikeyUsersREC <tauntrl@gmail.com>
2021-11-01 03:39:57 +0000

    Updated Profile.js

--- src/Commands/MeaxisNetwork/Profile.js
+++ src/Commands/MeaxisNetwork/Profile.js
@@ -39,7 +39,7 @@ module.exports = class extends Command {
             if (key !== null && key !== undefined && key !== 'titles' && value !== null) {
                 console.log([key, typeof(key)])
                 console.log([value, typeof(value)])
-                Embed.addField({ name: `${key[0].toLowerCase()}${key.split(1)}`, value: `${toString(value)}`, inline: false })
+                Embed.addField({ name: `${key}`, value: `${toString(value)}`, inline: false })
                 console.log(Embed)
 
             }
