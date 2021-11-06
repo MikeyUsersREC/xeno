b20c40f2f2012c89c7a0f4c521b8c5f977e53e1f
MikeyUsersREC <tauntrl@gmail.com>
2021-11-06 18:53:53 +0000

    Patch to Commands

--- src/Commands/MeaxisNetwork/Profile.js
+++ src/Commands/MeaxisNetwork/Profile.js
@@ -39,7 +39,7 @@ module.exports = class extends Command {
             if (key !== null && key !== undefined && key !== 'titles' && value !== null) {
                 console.log([key, typeof(key)])
                 console.log([value, typeof(value)])
-                Embed.addField(`${Utils.capitalize(key)}` ?? "Could not find variable", value.toString() ?? "null")
+                Embed.addField(`${key}` ?? "Could not find variable", value.toString() ?? "null")
                 console.log(Embed)
 
             }
