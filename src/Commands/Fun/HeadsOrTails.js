909cba47e55962f3260cb9b8434a3ca7ffa7c9c6
MikeyUsersREC <tauntrl@gmail.com>
2021-11-07 20:12:55 +0000

    Changes to RandomNumberGenerator.js

--- src/Commands/Fun/HeadsOrTails.js
+++ src/Commands/Fun/HeadsOrTails.js
@@ -30,8 +30,8 @@ module.exports = class extends Command {
 
         let msg = message.channel.send({ embeds: [Embed]})
 
+        msg.edit({ embeds: [Embed.addField('Result', `${picked}!`)]})
         const editMessage = () => [
-            msg.edit({ embeds: [Embed.addField('Result', `${picked}!`)]})
         ]
 
         setTimeout(editMessage(), 5000)
