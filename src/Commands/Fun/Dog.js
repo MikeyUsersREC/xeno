34e36cfd04e6db960b39b3a32d43839ce1812a77
MikeyUsersREC <tauntrl@gmail.com>
2021-11-09 22:39:28 +0000

    Awaiting everything

--- src/Commands/Fun/Dog.js
+++ src/Commands/Fun/Dog.js
@@ -33,4 +33,5 @@ module.exports = class extends Command {
         } else {
             return await message.channel.send({ content: 'An unknown error occured. Try running this command again and if this continues, contact Mikey#8970 for further support.'})
         }
+    }
 }
