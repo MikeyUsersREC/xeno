33d609036370c62980419b25ff1be180816411fa
MikeyUsersREC <tauntrl@gmail.com>
2021-11-09 22:38:37 +0000

    Awaiting everything

--- src/Commands/Fun/Cat.js
+++ src/Commands/Fun/Cat.js
@@ -26,9 +26,10 @@ module.exports = class extends Command {
             let selected = data[Math.floor(Math.random() * data.length)]
 
 
-        if (selected)
-            return await message.channel.send({ content: `https://imgur.com/${selected.hash}${selected.ext.replace(/\?.*/, '')}`})
-        else {
-            return await message.channel.send({ content: 'An unknown error occured. Try running this command again and if this continues, contact Mikey#8970 for further support.'})
-        }    }
-}
+            if (selected) {
+                return await message.channel.send({ content: `https://imgur.com/${selected.hash}${selected.ext.replace(/\?.*/, '')}`})
+            } else {
+                return await message.channel.send({ content: 'An unknown error occured. Try running this command again and if this continues, contact Mikey#8970 for further support.'})
+            }
+    }
+    }
