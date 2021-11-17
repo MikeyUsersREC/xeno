07d3b9d699329946ed70604a0164af187f07eef1
MikeyUsersREC <tauntrl@gmail.com>
2021-11-17 18:24:23 +0000

    ServerStartUp.js fixes

--- src/Commands/Roblox/ServerStartUp.js
+++ src/Commands/Roblox/ServerStartUp.js
@@ -32,7 +32,7 @@ module.exports = class extends Command {
         
         let ssuChannel;
 
-        let channels = message.guild.channels.cache.filter((channel) => { channel.type === 'GUILD_TEXT'}).array()
+        let channels = message.guild.channels.cache.filter((channel) => { channel.type === 'GUILD_TEXT'})
 
         const components = (state) => [
             new Discord.MessageActionRow().addComponents(
